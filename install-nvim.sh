041527d1c199a1ad9dd0861837ffe7f906e5d0a6
Joongi Kim <me@daybreaker.info>
2025-07-13 07:02:17 +0000

    Ensure ripgrep installed with nvim-telescope

--- install-nvim.sh
+++ install-nvim.sh
@@ -10,6 +10,7 @@ sudo cp nvim-linux-${ARCH}.appimage "${NVIM_PATH}"
 git clone --depth=1 https://github.com/stevearc/oil.nvim ~/.local/share/nvim/site/pack/oil/start/oil.nvim
 git clone --depth=1 https://github.com/nvim-lua/plenary.nvim ~/.local/share/nvim/site/pack/plenary/start/plenary.nvim
 git clone --depth=1 https://github.com/nvim-telescope/telescope.nvim ~/.local/share/nvim/site/pack/telescope/start/telescope.nvim
+sudo apt install -y ripgrep
 
 # Set nvim as the default editor (Ubuntu)
 sudo update-alternatives --install /usr/bin/vi vi "${NVIM_PATH}" 110
