ca591007eaf624af94c889a415f1593392be7809
Joongi Kim <me@daybreaker.info>
2025-07-13 06:42:46 +0000

    Update install-lsd.sh

--- install-lsd.sh
+++ install-lsd.sh
@@ -7,5 +7,5 @@ ARCH=$(dpkg --print-architecture)
 wget https://github.com/lsd-rs/lsd/releases/download/v1.1.5/lsd_1.1.5_${ARCH}.deb
 sudo dpkg -i lsd_1.1.5_${ARCH}.deb
 
-# Set lsd as the default ls (Ubuntu)
-sudo update-alternatives --install /usr/bin/ls ls "/usr/bin/lsd" 110
+# .profile.append has `alias ls=lsd` configuration.
+# I do not recommend replacing the original `ls` command system-wide to prevent unexpected breakage of various shell scripts.
